ea18b69259f57ee30e3c5fc1a6399ffb480deda2
Ismael <ismael.lajud65@gmail.com>
2024-07-24 18:17:24 +0200

    actulianzando

--- src/Views/ProductsGallery.jsx
+++ src/Views/ProductsGallery.jsx
@@ -1,83 +1,77 @@
 import axios from 'axios'
 import { useState, useEffect } from 'react'
 import Loading from '../Components/Loading'
-
+import Aos from 'aos'
+import 'aos/dist/aos.css'
 import { Col, Row } from 'react-bootstrap'
 import CardAllProducts from '../Components/CardAllProducts'
 
-
 const ProductsGallery = () => {
-    const [products, setProducts] = useState([])
+  const [products, setProducts] = useState([])
 
-    const [loading, setLoading] = useState(true)
+  const [loading, setLoading] = useState(true)
 
-    const getAllProducts = async () => {
-        setLoading(true)
-        try {
-            const response = await axios.get(`http://localhost:8091/api/productos/getAvailableProducts`)
-            setProducts(response.data)
+  const getAllProducts = async () => {
+    setLoading(true)
+    try {
+      const response = await axios.get('http://localhost:8091/api/productos/getAvailableProducts')
+      setProducts(response.data)
 
-            setLoading(false)
-
-        } catch (error) {
-            console.log("error to find products", error)
-            setLoading(false)
-        }
-
+      setLoading(false)
+    } catch (error) {
+      console.log('error to find products', error)
+      setLoading(false)
     }
+  }
 
+  useEffect(() => {
+    getAllProducts()
+  }, [])
 
+  useEffect(() => {
+    Aos.init({
+      duration: 2000
+    })
+  }, [])
 
-    useEffect(() => {
-        getAllProducts()
-    }, [])
-
-
-
-
-
-
-
-    return (
+  return (
         <div className='container-products'>
 
             {
-                loading ?
-                    (
+                loading
+                  ? (
                         <div style={{ display: 'flex', justifyContent: 'center', textAlign: 'center', marginTop: '150px' }}>
                             <Loading />
                         </div>
-                    ) :
+                    )
 
-                    (
+                  : (
                         <>
                             <div className='div-textheader'>
                                 <h3 className='text-header'>{products.length} PRODUCTOS</h3>
 
                             </div>
 
-                            <Row>
+                            <Row >
                                 {
 
                                     products.map((product) => (
-                                        <Col key={product._id} className='col-md-3'>
+                                        <Col
+
+                                        data-aos="fade-right" key={product._id} className='col-md-3'>
                                             <CardAllProducts prod={product}></CardAllProducts>
                                         </Col>
                                     ))
-
 
                                 }
                             </Row>
                         </>
                     )
 
-
             }
 
-
         </div>
-    )
+  )
 }
-
 
 export default ProductsGallery
